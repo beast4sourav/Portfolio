06a63a62c95388159d176a6774a3fe3b2099e8f0
sourav <souravkumar59139@gmail.com>
2024-10-27 21:33:02 +0530

    new commit

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -34,31 +34,6 @@ const portfolioProjects = [
     link: "https://gemini-sk.vercel.app/",
     image:gemini,
   },
-  
-  {
-    company: "Innovative Co",
-    year: "2021",
-    title: "Light Saas Landing Page",
-    results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
-    ],
-    link: "https://youtu.be/7hi5zwO75yc",
-    image: lightSaasLandingPage,
-  },
-  {
-    company: "Quantum Dynamics",
-    year: "2023",
-    title: "AI Startup Landing Page",
-    results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
-    ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
-    image: aiStartupLandingPage,
-  },
 
 ];
 
